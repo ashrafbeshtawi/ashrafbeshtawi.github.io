bba64eec05fd6766919308edaa88d8ac225f334e
ashrafbeshtawi <beshtawi.ashraf@gmail.com>
2021-04-18 16:11:06 +0200

    browsertype

---
+++ browserType/browser.js
@@ -0,0 +1,19 @@
+class BrowserType {
+    constructor() {
+        // OS
+        this.operatingSystem=navigator.platform;
+        // general info
+        this.general=window.navigator.userAgent;
+        //check if firefox 
+        this.firefox = typeof(InstallTrigger)!="undefined"? true : false;
+        //check if chrome
+        this.chrome = !!window.chrome;
+
+        this.IE = false;
+        this.edge = false;
+        this.opera = false;
+        this.safari = false;
+
+    }
+
+}
