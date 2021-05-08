fa3e39c89d24c8ed70f123681e24e46adc52686e
ashrafbeshtawi <beshtawi.ashraf@gmail.com>
2021-05-08 19:32:38 +0200

    JND integrated

---
+++ sample/assets/js/jnd_brightness.js
@@ -0,0 +1,124 @@
+// this script generate and validates the JND in the quality assessment task
+
+
+//reading configs for brightness
+//format and path
+let format_brightness_questions=config.brightness.format;
+let path_brightness_questions=config.brightness.path;
+//the perctnage of correct answers requested to pass the brightness test
+let correct_answers_requested_brightness=config.brightness.correct_answers_requested;
+
+//select target html element
+let target_brightness_questions=document.getElementById("JND_brightness");
+
+//check if the test was passed 
+let jnd_brightness_cookies=readCookie("jnd_brightness");
+
+//if the test was already done beofre and not expired yet then hide this test 
+if(jnd_brightness_cookies!=null){
+    document.getElementById("jnd_brightness_whole").setAttribute("style","display:none;");
+}else{
+    //the snr to be used to create the test
+    let ALL_SNR=config.brightness.snr;
+    // the numbers availabe in the sample
+    let number_samples=config.brightness.num_array;
+    //adding the questions
+    for (let index = 0; index < ALL_SNR.length; index++) {
+        // selecting number from the sample randomly
+        let sample_num=number_samples[Math.floor(Math.random() * number_samples.length)];
+        let current_correct_answer=sample_num;
+        //converting the number to the filename format of the samples
+        let image_name=`${ALL_SNR[index]}_${sample_num}`;
+
+        target_brightness_questions.innerHTML+= getBrightnessQuestion(image_name,index+1,current_correct_answer); 
+        
+    }
+    //add the validation button
+    target_brightness_questions.innerHTML+= `
+    <div class="question">
+        <button id="brightness_test_button" type="button" class="btn btn-primary" onclick="getValidationButton_Brightness()">Validate The Answers</button>
+    </div>
+    `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// validating the questions
+function getValidationButton_Brightness() {
+    // total number of questions
+    let q_num=config.brightness.snr.length;
+    let correct_answers=0;
+    // loop over the questions
+    for (let index = 0; index < q_num; index++) {
+        //selecting a question
+        const element = document.getElementById(`brightness_${index+1}`);
+        //get the correct answer
+        const correct_answer=element.getAttribute("correct");
+        //select the textbox
+        const text_box=document.getElementById(`brightness_${index+1}_${correct_answer}`);
+
+        // check if the correct answer is checked
+        if(text_box.value ==correct_answer){
+            correct_answers++;
+        }   
+    }
+
+    //deactivate the validation button
+    document.getElementById("brightness_test_button").disabled =true;
+    // check 
+    if(correct_answers>=correct_answers_requested_brightness){
+        target_brightness_questions.innerHTML+= `
+            <div class="alert alert-success" role="alert">
+            Test passed. Please continue
+            </div>
+        `;
+        createCookie("jnd_brightness", "done", config.showSetupEveryMinutes);
+
+    }else{
+        target_brightness_questions.innerHTML+= `
+            <div class="alert alert-danger" role="alert">
+            Test failed. Your screen brightness is too low<br>
+            Please adjest the screen brightness and refresh the page.
+            </div>
+        `
+    }
+
+    
+}
+
+// add the brightness question
+function getBrightnessQuestion(img,question_number,correct_answer) {
+console.log(`Brightness test: Question ${question_number} Debug Info: SNR: ${img} Correct Answer: ${correct_answer}`)
+return  `
+<div class="panel panel-info">
+    <div class="panel-heading">${question_number}# Question</div>
+    <div class="row_label">
+        <div class="colom_label">
+        <label> Image A</label> 
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="colom">
+        <img src="${path_brightness_questions+"/"+img+"."+format_brightness_questions}"  width="500" height="500">
+        </div>
+    </div>
+    <div class="question" id="brightness_${question_number}" correct="${correct_answer}">
+        <div class="input_question">
+            <label  for="brightness_${question_number}_${correct_answer}"> &nbsp; The Number: </label>
+            <input type="input" class="form-control" id="brightness_${question_number}_${correct_answer}" name="brightness_${question_number}_${correct_answer}" value="">
+        </div>
+    </div>
+</div>
+`;
+}
---
+++ sample/assets/js/jnd_distance.js
@@ -0,0 +1,162 @@
+// this script generate and validates the JND in the quality assessment task
+
+
+//reading configs for distance (blur)
+//format and path
+let format_blur_questions=config.blur.format;
+let path_blur_questions=config.blur.path;
+//the perctnage of correct answers requested to pass the blur test
+let correct_answers_requested=config.blur.correct_answers_requested;
+
+//select target html element
+let target_blur_questions=document.getElementById("JND_distance");
+
+//check if the test was passed 
+let jnd_distance_cookies=readCookie("jnd_distance");
+
+//if the test was not done before or result expired
+if(jnd_distance_cookies!=null){
+    document.getElementById("jnd_distance_whole").setAttribute("style","display:none;");
+}else{
+    
+    let ALL_SNR=config.blur.snr_pairs;
+        //adding the questions
+    for (let index = 0; index < ALL_SNR.length; index++) {
+
+        let first_image;
+        let second_image;
+        //shuffle images
+        if(Math.floor(Math.random() * 2)==0){
+            first_image=ALL_SNR[index][0];
+            second_image=ALL_SNR[index][1];
+        }else{
+            first_image=ALL_SNR[index][1];
+            second_image=ALL_SNR[index][0];
+        }
+        // detecting the correct answer
+        let current_correct_answer;
+        if (first_image>second_image){
+            current_correct_answer=1;
+        }else if(first_image<second_image){
+            current_correct_answer=2;
+        }else{
+            current_correct_answer=3;
+        }
+
+        target_blur_questions.innerHTML+= getDistanceQuestion(first_image,second_image,index+1,current_correct_answer); 
+        
+    }
+    //add the validation button
+    target_blur_questions.innerHTML+= `
+    <div class="question">
+        <button id="blur_test_button" type="button" class="btn btn-primary" onclick="getValidationButton()">Validate The Answers</button>
+    </div>
+    `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// validating the questions
+function getValidationButton() {
+    // total number of questions
+    let q_num=config.blur.snr_pairs.length;
+    let correct_answers=0;
+    // loop over the questions
+    for (let index = 0; index < q_num; index++) {
+        //selecting a question
+        const element = document.getElementById(`blur_${index+1}`);
+        //get the correct answer
+        const correct_answer=element.getAttribute("correct");
+        //select the correct radio buttons
+        
+        //find the id of the correct radio
+        let radio_id;
+        if(correct_answer==1){
+            radio_id="first";
+        }else if(correct_answer==2){
+            radio_id="second";
+        }else{
+            radio_id="third";
+        }
+        const radio_button=document.getElementById(`blur_${index+1}_${radio_id}`);
+
+        // check if the correct answer is checked
+        if(radio_button.checked){
+            correct_answers++;
+        }   
+    }
+
+    //deactivate the validation button
+    document.getElementById("blur_test_button").disabled =true;
+    // check 
+    if(correct_answers>=correct_answers_requested){
+        target_blur_questions.innerHTML+= `
+            <div class="alert alert-success" role="alert">
+            Test passed. Please continue
+            </div>
+        `;
+        createCookie("jnd_distance", "done", config.showSetupEveryMinutes);
+
+    }else{
+        target_blur_questions.innerHTML+= `
+            <div class="alert alert-danger" role="alert">
+            Test failed. Your distance from the screen should be ${config.blur.min_allowed_distance} to ${config.blur.max_allowed_distance} meters from the screen. <br>
+            Please refresh the page and do the test again.
+            </div>
+        `
+    }
+
+    
+}
+
+// add the blur question
+function getDistanceQuestion(snr1,snr2,question_number,correct_answer) {
+console.log(`Blur test: Question ${question_number} Debug Info: SNR: ${snr1},${snr2} Correct Answer: ${correct_answer}`)
+return  `
+<div class="panel panel-info">
+    <div class="panel-heading">${question_number}# Question</div>
+    <div class="row_label">
+        <div class="colom_label">
+        <label> Image A</label> 
+        </div>
+        <div class="colom_label">
+        <label> Image B</label> 
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="colom">
+        <img src="${path_blur_questions+"/"+snr1+"."+format_blur_questions}"  width="400" height="280">
+        </div>
+        <div class="colom">
+        <img src="${path_blur_questions+"/"+snr2+"."+format_blur_questions}"  width="400" height="280">
+        </div>
+    </div>
+    <div class="question" id="blur_${question_number}" correct="${correct_answer}">
+        <div class="radio_question">
+            <input type="radio" id="blur_${question_number}_first" name="blur_${question_number}_first" value="first">
+            <label  for="blur_${question_number}_first"> &nbsp; Quality of Image A is better.</label>
+        </div>
+
+        <div class="radio_question">
+        <input type="radio" id="blur_${question_number}_second" name="blur_${question_number}_second" value="second">
+        <label  for="blur_${question_number}_second" >&nbsp;Quality of Image B is better.</label>
+        </div>
+        <div class="radio_question">
+        <input type="radio" id="blur_${question_number}_undetectable" name="blur_${question_number}_undetectable" value="undetectable">
+        <label for="blur_${question_number}_undetectable" >&nbsp; Difference is not detectable.</label>
+        </div>
+    </div>
+</div>
+`;
+}
